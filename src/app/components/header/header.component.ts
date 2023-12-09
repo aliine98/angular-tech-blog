0902847e5f6a87bb1dc8364476e6696f58ea8a94
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-12-09 17:28:46 -0300

    ✨ feat: header bg style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  headerStyle: string = '';
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    window.scrollY > 10 ? this.headerStyle = 'bg-neutral-950/70' : this.headerStyle = 'bg-transparent';
+  }
 }
