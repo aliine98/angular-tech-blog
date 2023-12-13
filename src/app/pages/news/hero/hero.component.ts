f0ebfad438f89f33c542c6b1bdbfe43d4ce97bee
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-12-13 19:53:11 -0300

    ✨ feat: add live date

--- src/app/pages/news/hero/hero.component.ts
+++ src/app/pages/news/hero/hero.component.ts
@@ -1,7 +1,10 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-hero',
     templateUrl: './hero.component.html',
 })
-export class HeroComponent {}
+export class HeroComponent {
+    liveDate: string = formatDate(new Date(),'fullDate','en');
+}
