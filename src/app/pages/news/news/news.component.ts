088c017d30807f3bb6135bd2591b127565fe34a5
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-01-03 21:11:34 -0300

    ♻️ refactor: call getNews method on init

--- src/app/pages/news/news/news.component.ts
+++ src/app/pages/news/news/news.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewsData, NewsResult } from 'src/app/models/NewsData';
 import { NewsService } from 'src/app/services/news.service';
 
@@ -6,15 +6,18 @@ import { NewsService } from 'src/app/services/news.service';
     selector: 'app-news',
     templateUrl: './news.component.html',
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
     newsData: NewsData = {results: []};
 
     constructor(private newsService: NewsService) {
+    }
+
+    ngOnInit(): void {
         this.getNews();
     }
 
     getNews() {
-        this.newsService.getNews().subscribe({
+        this.newsService.getNewsList().subscribe({
             next: (res: NewsData) => {
                 this.newsData.results = res.results;
             }
