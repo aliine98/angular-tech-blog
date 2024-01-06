c97262be401c52acc79eb4dc04ebc1428bcd89be
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-01-05 21:44:49 -0300

    ✨ feat: method to get a news by id

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { NewsData } from '../models/NewsData';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,14 @@ export class NewsService {
   constructor(private http: HttpClient) {
   }
 
-  getNews(): Observable<NewsData> {
+  getNewsList(): Observable<NewsList> {
     return this.http.get<NewsData>(`https://newsdata.io/api/1/news?apikey=${environment.NEWS_API_KEY}&category=technology&language=en`);
     //this.http.get<NewsData>(`https://newsapi.org/v2/top-headlines?apiKey=${environment.NEWS_API_KEY}&category=technology&country=us`).subscribe(res => console.log(res));
+  }
+
+  getNews(id: string) {
+    return this.getNewsList().pipe(
+      map((newsData: NewsData) => newsData.find(news => news.article_id === Number(id)))
+    );
   }
 }
